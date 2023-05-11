d1b891153dadeaba0e8739347e27a1c27a1341b9
庞元贵 <89370828+YOUNDGREE-PANG@users.noreply.github.com>
2023-05-11 16:58:45 +0800

    Add files via upload

---
+++ application/extra/site.php
@@ -0,0 +1,44 @@
+<?php
+
+return array (
+  'name' => '中移骏和',
+  'beian' => '粤ICP备15000000号-1',
+  'cdnurl' => '',
+  'version' => '1.0.21',
+  'timezone' => 'Asia/Shanghai',
+  'forbiddenip' => '',
+  'languages' => 
+  array (
+    'backend' => 'zh-cn',
+    'frontend' => 'zh-cn',
+  ),
+  'fixedpage' => 'missionlists',
+  'categorytype' => 
+  array (
+    'default' => 'Default',
+    'page' => 'Page',
+    'article' => 'Article',
+    'test' => 'Test',
+  ),
+  'configgroup' => 
+  array (
+    'basic' => 'Basic',
+    'email' => 'Email',
+    'dictionary' => 'Dictionary',
+    'user' => 'User',
+    'example' => 'Example',
+  ),
+  'mail_type' => '1',
+  'mail_smtp_host' => 'smtp.qq.com',
+  'mail_smtp_port' => '465',
+  'mail_smtp_user' => '10000',
+  'mail_smtp_pass' => 'password',
+  'mail_verify_type' => '2',
+  'mail_from' => '10000@qq.com',
+  'attachmentcategory' => 
+  array (
+    'category1' => 'Category1',
+    'category2' => 'Category2',
+    'custom' => 'Custom',
+  ),
+);
