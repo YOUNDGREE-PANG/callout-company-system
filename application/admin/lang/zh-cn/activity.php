68749e82faaf2b16a5708fa70bddc8a25c009892
庞元贵 <89370828+YOUNDGREE-PANG@users.noreply.github.com>
2023-05-11 15:15:21 +0800

    Create activity.php

---
+++ application/admin/lang/zh-cn/activity.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Num'        => '最大参与人数',
+    'Start_time' => '起始时间',
+    'End_time'   => '截止时间',
+    'Miss_time'  => '兑奖失效日期',
+    'Adr'  => '可参与范围',
+    'Canuse'  => '活动状态',
+    'Rules'=>'活动规则'
+];
